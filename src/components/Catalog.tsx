864d56005cda44425a7336324e0324b398c60698
Matheus Rian <souzamatheusrian@gmail.com>
2022-09-22 21:21:06 -0300

    feat: add call to API in Catalog

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,10 +1,26 @@
-import { useSelector } from "react-redux"
+import { useEffect, useState } from 'react';
+import { Product } from "../store/modules/cart/models";
+import api from "../services/api";
 
 export function Catalog() {
-  const state = useSelector(state => state);
+  const [catalogs, setCatalogs] = useState<Product[]>([]);
 
-  console.log(state);
+  useEffect(() => {
+    api.get('products')
+      .then(response => setCatalogs(response.data))
+  }, []);
+
   return (
-    <h1>Catalog</h1>
+    <main>
+      <h1>Catalog</h1>
+      {catalogs.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> { " - " }
+          <span>{product.price}</span> { "  " }
+
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
   )
 }
