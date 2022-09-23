7994e69c29706f8f4d482f13a37b42dde42beaed
Matheus Rian <souzamatheusrian@gmail.com>
2022-09-22 21:44:25 -0300

    feat: create method handle add Product

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,13 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Product } from "../store/modules/cart/models";
 import api from "../services/api";
+import { useDispatch } from 'react-redux';
+import { addProductToCart } from '../store/modules/cart/actions';
 
 export function Catalog() {
+  const dispatch = useDispatch()
   const [catalogs, setCatalogs] = useState<Product[]>([]);
 
   useEffect(() => {
     api.get('products')
       .then(response => setCatalogs(response.data))
+  }, []);
+
+  const handleAddProductToCart = useCallback((product: Product) => {
+    dispatch(addProductToCart(product));
   }, []);
 
   return (
@@ -18,7 +25,7 @@ export function Catalog() {
           <strong>{product.title}</strong> { " - " }
           <span>{product.price}</span> { "  " }
 
-          <button type="button">Comprar</button>
+          <button type="button" onClick={() => handleAddProductToCart(product)}>Comprar</button>
         </article>
       ))}
     </main>
