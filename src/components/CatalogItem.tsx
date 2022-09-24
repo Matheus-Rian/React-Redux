398eb0703ac2d566b8df615b22dcd1bffd3b98d6
Matheus Rian <souzamatheusrian@gmail.com>
2022-09-23 21:39:36 -0300

    feat: create Component CatalogItem

---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,25 @@
+import { useCallback } from 'react';
+import { addProductToCart } from '../store/modules/cart/actions';
+import { Product } from '../store/modules/cart/models';
+import { useDispatch } from 'react-redux';
+
+interface CatalogItemProps {
+  product: Product
+}
+
+export const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const handleAddProductToCart = useCallback(() => {
+    dispatch(addProductToCart(product));
+  }, [dispatch, product]);
+
+  return (
+    <article>
+      <strong>{product.title}</strong> { " - " }
+      <span>{product.price}</span> { "  " }
+
+      <button type="button" onClick={handleAddProductToCart}>Comprar</button>
+    </article>
+  );
+}
