5fa7b8222894f9cec767dac1a77ea1d30283eaa8
Matheus Rian <souzamatheusrian@gmail.com>
2022-09-23 21:05:40 -0300

    feat: add types store redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,10 @@
 import { createStore } from "redux";
+import { CartState } from "./modules/cart/models";
 import rootReducers from "./modules/rootReducers";
+
+export interface IStateStore {
+  cart: CartState,
+}
 
 const store = createStore(rootReducers);
 
