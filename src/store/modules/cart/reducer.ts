d37819dfc58f5071234829e01c4705d408b8c9f7
Matheus Rian <souzamatheusrian@gmail.com>
2022-09-23 21:26:23 -0300

    feat: add verify if product already exists in cart

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,6 +1,10 @@
 import { Reducer } from "redux";
-import { CartState } from "./models";
+import { CartState, ItemCart, Product } from './models';
 import produce from "immer";
+
+interface actionPayload {
+  product: Product;
+}
 
 const INITIAL_STATE: CartState = {
   items: [],
@@ -10,12 +14,14 @@ const cart: Reducer<CartState> = (state = INITIAL_STATE, action) => {
   return produce(state, (draft) => {
     switch (action.type) {
       case "ADD_PRODUCT_TO_CART": {
-        const { product } = action.payload;
+        const { product } = action.payload as actionPayload;
 
-        draft.items.push({
-          product,
-          quantity: 1,
-        });
+        const productInCartIndex = draft.items.findIndex((item: ItemCart) => item.product.id === product.id);
+    
+        if (productInCartIndex >= 0)
+          draft.items[productInCartIndex].quantity++;
+        else
+          draft.items.push({ product, quantity: 1 });
       
         break;
       }
