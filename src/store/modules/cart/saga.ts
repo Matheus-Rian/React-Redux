a1fe6fe231acea83830d54d0bfa90b91f0be9b3f
Matheus Rian <souzamatheusrian@gmail.com>
2022-09-26 20:21:00 -0300

    feat: add verification currentlyQuantity

--- src/store/modules/cart/saga.ts
+++ src/store/modules/cart/saga.ts
@@ -1,11 +1,17 @@
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, select } from 'redux-saga/effects';
+import { IStateStore } from '../..';
 import { addProductToCart } from './actions';
 
 type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
 
-function checkProductStock({ payload }: CheckProductStockRequest) {
-  console.log(payload);
-  console.log('Adicionou ao carrinho');
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentlyQuantity: number = yield select((state: IStateStore) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  });
+
+  console.log(currentlyQuantity);
 }
 
 function* mySaga() {
